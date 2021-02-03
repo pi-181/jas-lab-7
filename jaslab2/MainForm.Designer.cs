11ed50500fac9398e137a79340ae58a646610b5a
demkom58 <demkom58@gmail.com>
2021-02-03 20:34:58 +0200

    Initial commit

---
+++ jaslab2/MainForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace jaslab2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.відкритиЗбіркуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(521, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {this.відкритиЗбіркуToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // відкритиЗбіркуToolStripMenuItem
+            // 
+            this.відкритиЗбіркуToolStripMenuItem.Name = "відкритиЗбіркуToolStripMenuItem";
+            this.відкритиЗбіркуToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.відкритиЗбіркуToolStripMenuItem.Text = "Відкрити збірку";
+            this.відкритиЗбіркуToolStripMenuItem.Click += new System.EventHandler(this.OnOpenBuildClick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.imageList1;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(225, 357);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.OnItemClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer) (resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "assembly-icon.png");
+            this.imageList1.Images.SetKeyName(1, "classes-icon.png");
+            this.imageList1.Images.SetKeyName(2, "enums-icon.png");
+            this.imageList1.Images.SetKeyName(3, "interface-icon.png");
+            this.imageList1.Images.SetKeyName(4, "private-fields-icon.png");
+            this.imageList1.Images.SetKeyName(5, "private-method-icon.png");
+            this.imageList1.Images.SetKeyName(6, "protected-fields-icon.png");
+            this.imageList1.Images.SetKeyName(7, "protected-method-icon.png");
+            this.imageList1.Images.SetKeyName(8, "public-fields-icon.png");
+            this.imageList1.Images.SetKeyName(9, "public-method-icon.png");
+            this.imageList1.Images.SetKeyName(10, "struct-icon.png");
+            this.imageList1.Images.SetKeyName(11, "private-contstructor-icon.png");
+            this.imageList1.Images.SetKeyName(12, "protected-constructor-icon.png");
+            this.imageList1.Images.SetKeyName(13, "public-constructor-icon.png");
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(0, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(517, 357);
+            this.splitContainer1.SplitterDistance = 225;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(288, 357);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 381);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "Java & C#: Lab №2";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.ListView listView1;
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        private System.Windows.Forms.ImageList imageList1;
+
+        private System.Windows.Forms.TreeView treeView1;
+
+        private System.Windows.Forms.ToolStripMenuItem відкритиЗбіркуToolStripMenuItem;
+
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+
+        #endregion
+    }
+}
---
+++ jaslab2/MainForm.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jaslab2
+{
+    public partial class MainForm : Form
+    {
+        private Assembly _assembly;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenBuildClick(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            string path = selectAssemblyFile();
+            if (path != null)
+            {
+                _assembly = openAssembly(path);
+            }
+
+            if (_assembly != null)
+            {
+                TreeNode root = new TreeNode {Text = _assembly.GetName().Name, ImageIndex = 0, SelectedImageIndex = 0};
+                treeView1.Nodes.Add(root);
+                Type[] types = _assembly.GetTypes();
+                addRoot(root, types);
+            }
+        }
+
+        private string selectAssemblyFile()
+        {
+            openFileDialog1.Filter = "Dll files (*.dll)|*.dll|Exe files (*.exe)|*.exe| All files (*.*)|*.*";
+            openFileDialog1.Title = "Select assembly file";
+            return (openFileDialog1.ShowDialog() == DialogResult.OK) ? openFileDialog1.FileName : null;
+        }
+
+        private Assembly openAssembly(string path)
+        {
+            try
+            {
+                Assembly a = Assembly.LoadFrom(path);
+                return a;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Не удалось загрузить указанную сборку!", 
+                    "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        void addRoot(TreeNode root, Type[] types)
+        {
+            foreach (Type type in types)
+            {
+                if (type.IsClass) addRootElement(root, type, 1);
+                else if (type.IsEnum) addRootElement(root, type, 2);
+                else if (type.IsInterface) addRootElement(root, type, 3);
+                else if (type.IsValueType) addRootElement(root, type, 10);
+            }
+        }
+
+        private void addRootElement(TreeNode root, Type type, int icon)
+        {
+            var node = new TreeNode {Tag = type, Text = type.ToString(), ImageIndex = icon, SelectedImageIndex = icon};
+            addFirstLevel(node, type);
+            root.Nodes.Add(node);
+        }
+
+        private void addFirstLevel(TreeNode node, Type type)
+        {
+            TreeNode node1;
+
+            var mods = ~BindingFlags.Default;
+            FieldInfo[] fields = type.GetFields(mods);
+            MethodInfo[] methods = type.GetMethods(mods);
+            ConstructorInfo[] constructors = type.GetConstructors(mods);
+
+            foreach (FieldInfo field in fields)
+            {
+                int icon = field.IsPrivate ? 4 : field.IsPublic ? 8 : 6; 
+                node1 = new TreeNode
+                {
+                    Tag = field, Text = field.FieldType.Name + " " + field.Name, ImageIndex = icon, SelectedImageIndex = icon
+                };
+                node.Nodes.Add(node1);
+            }
+
+            foreach (ConstructorInfo constructor in constructors)
+            {
+                int icon = constructor.IsPrivate ? 11 : constructor.IsPublic ? 13 : 12; 
+                String s = "";
+                ParameterInfo[] paramsInfo = constructor.GetParameters();
+                foreach (ParameterInfo param in paramsInfo)
+                {
+                    s = s + param.ParameterType.Name + ", ";
+                }
+
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode {Tag = constructor, Text = node.Text + "(" + s + ")", ImageIndex = icon, SelectedImageIndex = icon};
+                node.Nodes.Add(node1);
+            }
+
+            foreach (MethodInfo method in methods)
+            {
+                int icon = method.IsPrivate ? 5 : method.IsPublic ? 9 : 7; 
+                
+                String s = "";
+                ParameterInfo[] paramsInfo = method.GetParameters();
+                foreach (ParameterInfo param in paramsInfo)
+                {
+                    s = s + param.ParameterType.Name + ", ";
+                }
+
+                s = s.Trim();
+                s = s.TrimEnd(',');
+                node1 = new TreeNode
+                {
+                    Tag = method,
+                    Text = method.ReturnType.Name + " " + method.Name + "(" + s + ")",
+                    ImageIndex = icon,
+                    SelectedImageIndex = icon
+                };
+                node.Nodes.Add(node1);
+            }
+        }
+
+        private void OnItemClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            listView1.Clear();
+            
+            var tag = e.Node.Tag;
+            if (tag == null)
+            {
+                listView1.Items.Add(new ListViewItem("No info about this node!"));
+                return;
+            }
+            
+            if (tag is ConstructorInfo cInfo) 
+                DescribeConstructor(cInfo);
+            else if (tag is MethodInfo mInfo) 
+                DescribeMethod(mInfo);
+            else if (tag is FieldInfo fInfo) 
+                DescribeField(fInfo);
+            else if (tag is Type type) 
+                DescribeType(type);
+            listView1.Update();
+        }
+
+        private void DescribeType(Type type)
+        {
+            var items = listView1.Items;
+            items.Add("Назва: " + type.Name);
+            items.Add(" ");
+            
+            items.Add("Модифікатори доступу: " + (
+                          type.IsPublic ? "Публічний" : 
+                          type.IsNestedPublic ? "Nested Public" : 
+                          type.IsNestedPrivate ? "Nested Private" : "Приватний"));
+
+            items.Add("Enum: " + (type.IsEnum ? "Так" : "Ні"));
+            items.Add("Interface: " + (type.IsInterface ? "Так" : "Ні"));
+            items.Add("Abstract: " + (type.IsAbstract ? "Так" : "Ні"));
+            items.Add("Class: " + (type.IsClass ? "Так" : "Ні"));
+            items.Add("Serializable: " + (type.IsSerializable ? "Так" : "Ні"));
+            items.Add("Nested: " + (type.IsNested ? "Так" : "Ні"));
+            items.Add("ValueType (structure): " + (type.IsValueType ? "Так" : "Ні"));
+            items.Add("Transparent: " + (type.IsSecurityTransparent ? "Так" : "Ні"));
+            items.Add("SecurityCritical: " + (type.IsSecurityCritical ? "Так" : "Ні"));
+            items.Add("SecuritySafeCritical: " + (type.IsSecuritySafeCritical ? "Так" : "Ні"));
+        }
+        
+        private void DescribeConstructor(ConstructorInfo info)
+        {
+            var items = listView1.Items;
+            items.Add("Назва: " + info.Name);
+            items.Add("Параметри: ");
+            foreach (var pInfo in info.GetParameters())
+            {
+                items.Add( (pInfo.IsIn ? "In" : "Out") + " " + pInfo.ParameterType + " " + pInfo.Name);
+            }
+            items.Add(" ");
+            
+            items.Add("Constructor: " + (info.IsConstructor ? "Так" : "Ні"));
+            items.Add("Видимість: " + (info.IsStatic ? "Статичне" : "Локальне"));
+            items.Add("Модифікатори доступу: " 
+                      + (info.IsPublic ? "Публічне" : info.IsPrivate ? "Приватне" : "Непублічне"));
+            items.Add("Final: " + (info.IsFinal ? "Так" : "Ні"));
+            items.Add("Abstract: " + (info.IsAbstract ? "Так" : "Ні"));
+            items.Add("Assembly: " + (info.IsAssembly ? "Так" : "Ні"));
+            items.Add("Transparent: " + (info.IsSecurityTransparent ? "Так" : "Ні"));
+            items.Add("SecurityCritical: " + (info.IsSecurityCritical ? "Так" : "Ні"));
+            items.Add("SecuritySafeCritical: " + (info.IsSecuritySafeCritical ? "Так" : "Ні"));
+        }
+        
+        private void DescribeMethod(MethodInfo info)
+        {
+            var items = listView1.Items;
+            items.Add("Назва: " + info.Name);
+            items.Add("Повертаємий тип: " + info.ReturnType);
+            items.Add("Параметри: ");
+            foreach (var pInfo in info.GetParameters())
+            {
+                items.Add( (pInfo.IsIn ? "In" : "Out") + " " + pInfo.ParameterType + " " + pInfo.Name);
+            }
+            items.Add(" ");
+            
+            items.Add("Constructor: " + (info.IsConstructor ? "Так" : "Ні"));
+            items.Add("Видимість: " + (info.IsStatic ? "Статичне" : "Локальне"));
+            items.Add("Модифікатори доступу: " 
+                      + (info.IsPublic ? "Публічне" : info.IsPrivate ? "Приватне" : "Непублічне"));
+            items.Add("Final: " + (info.IsFinal ? "Так" : "Ні"));
+            items.Add("Abstract: " + (info.IsAbstract ? "Так" : "Ні"));
+            items.Add("Assembly: " + (info.IsAssembly ? "Так" : "Ні"));
+            items.Add("Transparent: " + (info.IsSecurityTransparent ? "Так" : "Ні"));
+            items.Add("SecurityCritical: " + (info.IsSecurityCritical ? "Так" : "Ні"));
+            items.Add("SecuritySafeCritical: " + (info.IsSecuritySafeCritical ? "Так" : "Ні"));
+        }
+        
+        private void DescribeField(FieldInfo info)
+        {
+            var items = listView1.Items;
+            items.Add("Назва: " + info.Name);
+            items.Add("Тип: " + info.FieldType);
+            items.Add(" ");
+
+            items.Add("Видимість: " + (info.IsStatic ? "Статичне" : "Локальне"));
+            items.Add("Модифікатори доступу: " 
+                      + (info.IsPublic ? "Публічне" : info.IsPrivate ? "Приватне" : "Непублічне"));
+            items.Add("Init: " + (info.IsInitOnly ? "Так" : "Ні"));
+            items.Add("Assembly: " + (info.IsAssembly ? "Так" : "Ні"));
+            items.Add("Transparent: " + (info.IsSecurityTransparent ? "Так" : "Ні"));
+            items.Add("Literal: " + (info.IsLiteral ? "Так" : "Ні"));
+            items.Add("SecurityCritical: " + (info.IsSecurityCritical ? "Так" : "Ні"));
+            items.Add("SecuritySafeCritical: " + (info.IsSecuritySafeCritical ? "Так" : "Ні"));
+        }
+        
+    }
+}
